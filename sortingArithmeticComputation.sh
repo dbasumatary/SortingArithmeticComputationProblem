3ba9dc9f8a419b2dd24bcb1bdfae0802f503e5e2
dbasumatary <basudhondip@gmail.com>
2023-01-20 21:13:12 +0530

    Add: Compute c + a / b

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -6,3 +6,5 @@ read -p "Enter third number: " c
 echo "The three numbers are: $a $b $c"
 echo "$a + $b * $c = $(($a + $b * $c))"
 echo "$a * $b + $c = $(($a * $b + $c))"
+value=`awk 'BEGIN{printf("%0.2f",'$c+$a/$b')}'`;
+echo "$c + $a / $b = $value"
