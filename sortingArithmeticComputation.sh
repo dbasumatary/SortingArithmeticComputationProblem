533ed54670b9e8f90fb42aba809e7eaa03e54540
dbasumatary <basudhondip@gmail.com>
2023-01-20 23:24:40 +0530

    Add: Store the results in a Dictionary for every Computation

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -3,9 +3,29 @@ echo "Welcome to Sorting Arithmetic Computation Problem"
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
-echo "The three numbers are: $a $b $c"
-echo "$a + $b * $c = $(($a + $b * $c))"
-echo "$a * $b + $c = $(($a * $b + $c))"
-value=`awk 'BEGIN{printf("%0.2f",'$c+$a/$b')}'`;
-echo "$c + $a / $b = $value"
-echo "$a % $b + $c = $(($a % $b + $c))"
+echo "The three numbers are: $a, $b and $c"
+
+firstCompute=$(( $a + $b * $c ))
+echo "$a + $b * $c = $firstCompute"
+
+secondCompute=$(( $a * $b + $c ))
+echo "$a * $b + $c = $secondCompute"
+
+thirdCompute=`awk 'BEGIN{printf("%0.2f",'$c+$a/$b')}'`;
+echo "$c + $a / $b = $thirdCompute"
+
+fourthCompute=$(( $a % $b + $c ))
+echo "$a % $b + $c = $fourthCompute"
+
+declare -A dictionary
+
+
+dictionary[firstCompute]=$firstCompute
+dictionary[secondCompute]=$secondCompute
+dictionary[thirdCompute]=$thirdCompute
+dictionary[fourthCompute]=$fourthCompute
+
+for computation in ${!dictionary[@]}
+do
+	echo "dictionary[$computation] = ${dictionary[$computation]}"
+done
